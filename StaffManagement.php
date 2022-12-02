5e14033175b25129932cb6e8afc8c1560c1b2fa7
Isaac Reyes <isaacjdjreyes@gmail.com>
2022-12-02 18:07:07 +0800

    Blur file instead of using filter

--- StaffManagement.php
+++ StaffManagement.php
@@ -15,21 +15,27 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dashboard</title>
+        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <div class="main">
-            <div class="navsection">
-            <div class="logo-container"></div>
-            <ul>
-                <li>Notifications</li>
-                <li>Messages</li>
-                <li>Logout</li>
-            </ul>
-        </div>
-        <div class="mainsection">
-            <h1>Welcome Back!</h1>
 
-        </div></div>
+        <div class="main">
+
+            <div class="navsection">
+                <div class="logo-container"></div>
+                <ul>
+                    <li>Notifications</li>
+                    <li>Messages</li>
+                    <li>Logout</li>
+                </ul>
+            </div>
+            
+            <div class="mainsection">
+                <h1>Welcome Back User!</h1>
+
+            </div>
+
+        </div>
         
     </body>
 </html>
