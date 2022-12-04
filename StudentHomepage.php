492f22ebbd583f5fb806e272de0d2409573d9d6f
Edriech Balajadia <edriechbalajadia@gmail.com>
2022-12-04 16:51:47 +0800

    fix course view

--- StudentHomepage.php
+++ StudentHomepage.php
@@ -1,135 +1,160 @@
 <?php
-    $conn = new mysqli("localhost:3310", "root", "mysql", "school");
-    if($conn -> connect_error) {
-        die ("Connect Error (".$conn->connect_Errorno.") ".$conn->connect_error);
-    }
-    $STU_ID = substr($_REQUEST["userName"], 4);
-    $ENRL_YEAR = substr($_REQUEST["userName"], 0, 4);
-    $sql = "SELECT * FROM school.course";
-    $result = $conn -> query($sql);
-    
-    $student_info_query = "SELECT * FROM student WHERE STU_ID = $STU_ID AND ENRL_YEAR = $ENRL_YEAR;";
-    $student_info_result = $conn -> query($student_info_query);
-    $student_info_row = $student_info_result -> fetch_assoc();
+$conn = new mysqli("localhost:3310", "root", "mysql", "school");
+if ($conn->connect_error) {
+    die("Connect Error (" . $conn->connect_Errorno . ") " . $conn->connect_error);
+}
+$STU_ID = substr($_REQUEST["userName"], 4);
+$ENRL_YEAR = substr($_REQUEST["userName"], 0, 4);
+$sql = "SELECT * FROM school.course";
+$result = $conn->query($sql);
 
-    $STU_EMAIL = $student_info_row["STU_EMAIL"];
-    $STU_FNAME = $student_info_row["STU_FNAME"];
-    $STU_MI = $student_info_row["STU_MI"];
-    $STU_LNAME = $student_info_row["STU_LNAME"];
-    $STU_GENDER = $student_info_row["STU_GENDER"];
-    $STU_BDAY = $student_info_row["STU_BDAY"];
-    $DEPT_ID = $student_info_row["DEPT_ID"];
-    $SPEC_ID = $student_info_row["SPEC_ID"];
+$student_info_query = "SELECT * FROM student WHERE STU_ID = $STU_ID AND ENRL_YEAR = $ENRL_YEAR;";
+$student_info_result = $conn->query($student_info_query);
+$student_info_row = $student_info_result->fetch_assoc();
 
-    // $add_stu_query = "INSERT INTO student(STU_ID, ENRL_YEAR, STU_FNAME, STU_MI, STU_LNAME, STU_BDAY, STU_GENDER, DEPT_ID, SPEC_ID, STU_EMAIL)
-	// VALUES ($STU_ID, $ENRL_YEAR, '$STU_FNAME', '$STU_MI', '$STU_LNAME', '$STU_BDAY', '$STU_GENDER', $DEPT_ID, $SPEC_ID, '$STU_EMAIL');";
-    // mysqli_query($conn, $add_stu_query);
+$STU_EMAIL = $student_info_row["STU_EMAIL"];
+$STU_FNAME = $student_info_row["STU_FNAME"];
+$STU_MI = $student_info_row["STU_MI"];
+$STU_LNAME = $student_info_row["STU_LNAME"];
+$STU_GENDER = $student_info_row["STU_GENDER"];
+$STU_BDAY = $student_info_row["STU_BDAY"];
+$DEPT_ID = $student_info_row["DEPT_ID"];
+$SPEC_ID = $student_info_row["SPEC_ID"];
 
-    $conn -> close();
+$info_query = "SELECT * FROM student AS s
+	INNER JOIN enrollment AS e
+		ON s.STU_ID = e.STU_ID AND s.ENRL_YEAR = e.ENRL_YEAR
+	INNER JOIN class AS c
+		ON e.CLASS_ID = c.CLASS_ID
+	INNER JOIN course AS co
+		ON c.CRS_ID = co.CRS_ID
+	INNER JOIN instructor AS i
+		ON c.INSTR_ID = i.INSTR_ID
+    INNER JOIN department AS d
+		ON s.DEPT_ID = d.DEPT_ID
+	INNER JOIN specialization AS sp
+		ON s.SPEC_ID = sp.SPEC_ID
+			WHERE s.STU_ID = $STU_ID AND e.ENRL_YEAR = $ENRL_YEAR;";
+
+$info_query_result = $conn->query($info_query);
+// $info_query_row = $info_query_result->fetch_assoc();
+
+$conn->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Enrollment</title>
-        <link rel="stylesheet" href="styles.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;800&display=swap" rel="stylesheet">
-    </head>
-    <script>
-        console.log("========== LOG ==========")
-        console.log(<?php echo $ENRL_YEAR . " " . $STU_ID ?>);
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enrollment</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;800&display=swap" rel="stylesheet">
+</head>
+<script>
+    console.log("========== LOG ==========")
+    console.log(<?php echo $ENRL_YEAR . " " . $STU_ID ?>);
+
     function openInfoForm() {
         document.getElementById("updateinfoform").style.display = "flex";
     }
+
     function closeInfoForm() {
         document.getElementById("updateinfoform").style.display = "none";
     }
+
     function openEnrollForm() {
         document.getElementById("enrollform").style.display = "flex";
     }
+
     function closeEnrollForm() {
         document.getElementById("enrollform").style.display = "none";
     }
-    </script>
-    <body>
-        <div class="navsection">
-            <div class="logo-container">
-                <img src="images/ustseal.png" width="50px">
-            </div>
-            <ul>
-                <li>Notifications</li>
-                <li>Messages</li>
-                <li><a href="Landing.html">Logout</a></li>
-            </ul>
+</script>
+
+<body>
+    <div class="navsection">
+        <div class="logo-container">
+            <img src="images/ustseal.png" width="50px">
         </div>
-        <div class="mainsection">
-            <h1><?= $STU_FNAME ?>'s Profile</h1>
-            <fieldset>
-                <table>
-                    <tr>
-                        <th>Student ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Department</th>
-                        <th>Specialization</th>
-                        <th>Birthday</th>
-                        <th>Gender</th>
-                        <th>Actions</th>
-                    </tr>
-                    <!-- Update this section to print student information from table with php -->
-                    <tr>
-                        <td><?= $ENRL_YEAR . $STU_ID ?></td>
-                        <td><?= $STU_FNAME . " " . $STU_LNAME ?></td>
-                        <td><?= $STU_EMAIL ?></td>
-                        <td><?= $DEPT_ID ?></td>
-                        <td><?= $SPEC_ID ?></td>
-                        <td><?= $STU_BDAY ?></td>
-                        <td><?= $STU_GENDER ?></td>
-                        <td>
-                            <button onclick="openInfoForm()" class="updateinfo-btn">Update</button>
-                        </td>
+        <ul>
+            <li>Notifications</li>
+            <li>Messages</li>
+            <li><a href="Landing.html">Logout</a></li>
+        </ul>
+    </div>
+    <div class="mainsection">
+        <h1><?= $STU_FNAME ?>'s Profile</h1>
+        <fieldset>
+            <table>
+                <tr>
+                    <th>Student ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Department</th>
+                    <th>Specialization</th>
+                    <th>Birthday</th>
+                    <th>Gender</th>
+                    <th>Actions</th>
+                </tr>
+                <!-- Update this section to print student information from table with php -->
+                <tr>
+                    <td><?= $ENRL_YEAR . $STU_ID ?></td>
+                    <td><?= $STU_FNAME . " " . $STU_LNAME ?></td>
+                    <td><?= $STU_EMAIL ?></td>
+                    <td><?= $DEPT_ID ?></td>
+                    <td><?= $SPEC_ID ?></td>
+                    <td><?= $STU_BDAY ?></td>
+                    <td><?= $STU_GENDER ?></td>
+                    <td>
+                        <button onclick="openInfoForm()" class="updateinfo-btn">Update</button>
+                    </td>
                     <!--  -->
-                    </tr>        
-                </table>
-            </fieldset>
+                </tr>
+            </table>
+        </fieldset>
 
-            <fieldset>
+        <fieldset>
             <legend>Enrolled Courses</legend>
-                <table>
+            <table>
+                <tr>
+                    <th>Course Name</th>
+                    <th>Course Description</th>
+                    <th>Course Level</th>
+                    <th>Course Department</th>
+                    <th>Class Section</th>
+                    <th>Course Units</th>
+                    <th>Class Instructor</th>
+                    <th>Actions</th>
+                </tr>
+                
+                <?php while ($info_query_row = $info_query_result->fetch_assoc()) {            ?>
                     <tr>
-                        <th>Course ID</th>
-                        <th>Course Name</th>
-                        <th>Course Description</th>
-                        <th>Course Level</th>
-                        <th>Course Units</th>
-                        <th>Actions</th>
+                        <td><?php echo $info_query_row["CRS_NAME"]; ?></td>
+                        <td><?php echo $info_query_row["CRS_DESC"]; ?></td>
+                        <td><?php echo $info_query_row["CRS_LEVEL"]; ?></td>
+                        <td><?php echo $info_query_row["DEPT_ID"]; ?></td>
+                        <td><?php echo $info_query_row["CLASS_SECTION"]; ?></td>
+                        <td><?php echo $info_query_row["CRS_UNIT"]; ?></td>
+                        <td><?php echo $info_query_row["INSTR_FNAME"]; ?></td>
+                        <td><button action="" class="removecrs-btn">Drop</button></td>
                     </tr>
-                    <!-- Update button to delete course from student courses -->
-                    <!-- Update this section to print enrolled courses with php -->
-                    <tr>
-                        <th>1</th>
-                        <th>ICS2602</th>
-                        <th>Computer Programming 1</th>
-                        <th>1</th>
-                        <th>5</th>
-                        <th><button action="" class="removecrs-btn">Drop</button></th>
-                    </tr>
-                    <!--  -->
-                </table>
-                <button onclick="openEnrollForm()" class="enrollcrs-btn">Enroll New Course +</button>
-            </fieldset>
+                <?php   }                                               ?>
+                <!--  -->
+            </table>
+            <button onclick="openEnrollForm()" class="enrollcrs-btn">Enroll New Course +</button>
+        </fieldset>
 
-            
-            <!-- Enroll Course Form Popup [Add form action to update student enrolled courses] -->
-            <form action="" method="POST" id="enrollform">
-                <fieldset>
+
+        <!-- Enroll Course Form Popup [Add form action to update student enrolled courses] -->
+        <form action="" method="POST" id="enrollform">
+            <fieldset>
                 <legend>Available Courses</legend>
-                    <table>
+                <table>
                     <tr>
                         <th>+</th>
                         <th>ID</th>
@@ -138,43 +163,44 @@
                         <th>Level</th>
                         <th>Units</th>
                     </tr>
-            <?php   while($row=$result->fetch_assoc()) {            ?>
-                    <tr>
-                        <td><input name="Courses[]" type="checkbox" value="<?php $row["CRS_ID"]?>"></td>
-                        <td><?php echo $row["CRS_ID"];?></td>
-                        <td><?php echo $row["CRS_NAME"];?></td>
-                        <td><?php echo $row["CRS_DESC"];?></td>
-                        <td><?php echo $row["CRS_LEVEL"];?></td>
-                        <td><?php echo $row["CRS_UNIT"];?></td>
-                    </tr>        
-            <?php   }                                               ?>
-                    </table>
-                    <div class="formBtns">
-                        <button onclick="closeEnrollForm()" class="enrollcrs-btn cancel">Close</button>
-                        <input type="submit" value="Enroll +" class="enrollcrs-btn" formaction="">
-                    </div>
-                </fieldset>
-            </form>
+                    <?php while ($row = $result->fetch_assoc()) {            ?>
+                        <tr>
+                            <td><input name="Courses[]" type="checkbox" value="<?php $row["CRS_ID"] ?>"></td>
+                            <td><?php echo $row["CRS_ID"]; ?></td>
+                            <td><?php echo $row["CRS_NAME"]; ?></td>
+                            <td><?php echo $row["CRS_DESC"]; ?></td>
+                            <td><?php echo $row["CRS_LEVEL"]; ?></td>
+                            <td><?php echo $row["CRS_UNIT"]; ?></td>
+                        </tr>
+                    <?php   }                                               ?>
+                </table>
+                <div class="formBtns">
+                    <button onclick="closeEnrollForm()" class="enrollcrs-btn cancel">Close</button>
+                    <input type="submit" value="Enroll +" class="enrollcrs-btn" formaction="">
+                </div>
+            </fieldset>
+        </form>
 
-            <!-- Update Info Form Popup [Add form action to update student information]--> 
-            <form action="" method="POST" name="myForm" id="updateinfoform">
-                <fieldset>
-                    <legend>Enter your personal information: </legend>
-                    <label>First Name: <input type="text" name="FNAME" required></label>
-                    <label>Middle Initial: <input type="text" name="MI" required></label>
-                    <label>Last Name: <input type="text" name="LNAME" required></label>
-                    <label>Gender: <select name="GENDER" required>
+        <!-- Update Info Form Popup [Add form action to update student information]-->
+        <form action="" method="POST" name="myForm" id="updateinfoform">
+            <fieldset>
+                <legend>Enter your personal information: </legend>
+                <label>First Name: <input type="text" name="FNAME" required></label>
+                <label>Middle Initial: <input type="text" name="MI" required></label>
+                <label>Last Name: <input type="text" name="LNAME" required></label>
+                <label>Gender: <select name="GENDER" required>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                     </select></label>
-                    <label>Date of Birth: <input type="date" name="BDAY" required></label>
-                    <div class="formBtns">
-                        <button onclick="closeInfoForm()" class="update-btn cancel">Close</button>
-                        <input type="submit" value="Update" class="update-btn" formaction="">
-                    </div>
-                </fieldset>
-            </form>
+                <label>Date of Birth: <input type="date" name="BDAY" required></label>
+                <div class="formBtns">
+                    <button onclick="closeInfoForm()" class="update-btn cancel">Close</button>
+                    <input type="submit" value="Update" class="update-btn" formaction="">
+                </div>
+            </fieldset>
+        </form>
 
-        </div>
-    </body>
+    </div>
+</body>
+
 </html>
