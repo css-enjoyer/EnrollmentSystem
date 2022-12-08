54686b643f7efedfeb60c9cae584a2655c50f2ab
Edriech Balajadia <edriechbalajadia@gmail.com>
2022-12-08 18:45:16 +0800

    fix student register

--- student/StudentServer.php
+++ student/StudentServer.php
@@ -1,7 +1,51 @@
-<?php 
+<?php
+require_once('./../config.php');
 session_start();
 
 // initialize variable in case of duplication
 $STU_EMAIL = "";
+$errors = array();
 
-?>
+$db = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
+
+// Register student
+if (isset($_POST['reg_stu'])) {
+    // Receive all inputs from form
+    $STU_FNAME = mysqli_real_escape_string($db, $_POST['STU_FNAME']);
+    $STU_MI = mysqli_real_escape_string($db, $_POST['STU_MI']);
+    $STU_LNAME = mysqli_real_escape_string($db, $_POST['STU_LNAME']);
+    $STU_GENDER = mysqli_real_escape_string($db, $_POST['STU_GENDER']);
+    $STU_BDAY = mysqli_real_escape_string($db, $_POST['STU_BDAY']);
+    $STU_ADDRESS = mysqli_real_escape_string($db, $_POST['STU_ADDRESS']);
+    $STU_CONTACT = mysqli_real_escape_string($db, $_POST['STU_CONTACT']);
+    $STU_PASSWORD_1 = mysqli_real_escape_string($db, $_POST['STU_PASSWORD_1']);
+    $STU_PASSWORD_2 = mysqli_real_escape_string($db, $_POST['STU_PASSWORD_2']);
+
+    if ($STU_PASSWORD_1 != $STU_PASSWORD_2) {
+        array_push($errors, "The two passwords do not match");
+    }
+
+    $stu_email_check_query = "SELECT * FROM student WHERE STU_EMAIL ='$STU_EMAIL' LIMIT 1";
+    $result = mysqli_query($db, $stu_email_check_query);
+    $stu_email = mysqli_fetch_assoc($result);
+
+    if ($stu_email) {
+        if ($stu_email['STU_EMAIL'] === $STU_EMAIL) {
+            array_push($errors, "Student email already exists");
+        }
+    }
+
+    // Finally, register student if there are no errors in the form
+    if (count($errors) == 0) {
+        $STU_PASSWORD_1 = md5($STU_PASSWORD_1); //encrypt the password before saving in the database
+
+        $query = "INSERT INTO 
+        student (STU_FNAME, STU_MI, STU_LNAME, STU_GENDER, STU_BDAY, STU_PASSWORD,
+                 STU_ADDRESS, STU_CONTACT, STU_EMAIL) VALUES ('$STU_FNAME', '$STU_MI', '$STU_LNAME', '$STU_GENDER', '$STU_BDAY', '$STU_PASSWORD_1',
+                 '$STU_ADDRESS', '$STU_CONTACT', '$STU_EMAIL');";
+        mysqli_query($db, $query);
+        $_SESSION['STU_FNAME'] = $STU_FNAME;
+        $_SESSION['success'] = "You are now logged in";
+        header('location: StudentHomepage.php');
+    }
+}
