49e08b167b5aa499309edb96fcf9d9713cff9ee6
Edriech Balajadia <edriechbalajadia@gmail.com>
2022-12-08 19:05:50 +0800

    fixes for stu reg

--- student/StudentServer.php
+++ student/StudentServer.php
@@ -8,9 +8,10 @@ $errors = array();
 
 $db = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
 
-// Register student
+// ---------- REGISTER STUDENT ----------
 if (isset($_POST['reg_stu'])) {
     // Receive all inputs from form
+    $STU_EMAIL = mysqli_real_escape_string($db, $_POST['STU_EMAIL']);
     $STU_FNAME = mysqli_real_escape_string($db, $_POST['STU_FNAME']);
     $STU_MI = mysqli_real_escape_string($db, $_POST['STU_MI']);
     $STU_LNAME = mysqli_real_escape_string($db, $_POST['STU_LNAME']);
@@ -25,6 +26,7 @@ if (isset($_POST['reg_stu'])) {
         array_push($errors, "The two passwords do not match");
     }
 
+    // Check if student email already exists
     $stu_email_check_query = "SELECT * FROM student WHERE STU_EMAIL ='$STU_EMAIL' LIMIT 1";
     $result = mysqli_query($db, $stu_email_check_query);
     $stu_email = mysqli_fetch_assoc($result);
@@ -37,15 +39,19 @@ if (isset($_POST['reg_stu'])) {
 
     // Finally, register student if there are no errors in the form
     if (count($errors) == 0) {
-        $STU_PASSWORD_1 = md5($STU_PASSWORD_1); //encrypt the password before saving in the database
+        // $STU_PASSWORD_1 = md5($STU_PASSWORD_1); // encrypt the password before saving in the database (optional)
 
         $query = "INSERT INTO 
         student (STU_FNAME, STU_MI, STU_LNAME, STU_GENDER, STU_BDAY, STU_PASSWORD,
                  STU_ADDRESS, STU_CONTACT, STU_EMAIL) VALUES ('$STU_FNAME', '$STU_MI', '$STU_LNAME', '$STU_GENDER', '$STU_BDAY', '$STU_PASSWORD_1',
                  '$STU_ADDRESS', '$STU_CONTACT', '$STU_EMAIL');";
         mysqli_query($db, $query);
-        $_SESSION['STU_FNAME'] = $STU_FNAME;
+        // Unique key passed to the session used to determine id
+        $_SESSION['STU_EMAIL'] = $STU_EMAIL;
         $_SESSION['success'] = "You are now logged in";
         header('location: StudentHomepage.php');
     }
 }
+
+// ----------- LOGIN STUDENT ----------
+
