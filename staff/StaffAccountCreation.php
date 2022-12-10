ec47e48f8261b1ca1d94bc1770da6761e8bc8cdc
Edriech Balajadia <edriechbalajadia@gmail.com>
2022-12-10 12:47:45 +0800

    fix close admin in landing page

--- staff/StaffAccountCreation.php
+++ staff/StaffAccountCreation.php
@@ -18,7 +18,7 @@ $conn->close();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign up</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./../styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;800&display=swap" rel="stylesheet">
